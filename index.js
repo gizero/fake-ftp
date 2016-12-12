9e4508c79a224f92efdbcc500cdefbbef8b5004f
Andrea Galbusera <gizero@gmail.com>
2016-12-12 07:07:06 +0100

    minimal alternative fs implementation
    * expose a fixed file list
    * implement an empty GET handler
    * add a couple of uninplemented functions hooks

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ server = new ftpd.FtpServer(options.host, {
   },
   getRoot: function() {
     return process.cwd();
-  }
+  },
+  useReadFile: true
 });
 
 server.on('error', function(err) {
@@ -33,7 +34,47 @@ server.on('client:connected', function(connection) {
 
   connection.on('command:pass', function(pass, success, failure) {
     if (pass) {
-      success(username);
+      success(username, {
+        readdir: function(path, cb) {
+          // always return the same file list
+          return cb(null, ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']);
+        },
+        stat: function(path, cb) {
+          // always return stats for a file with on-the-fly created timestamps
+          return cb(null, {
+            //dev: 2114,
+            //ino: 48064969,
+            //mode: 33188,
+            //nlink: 1,
+            //uid: 85,
+            //gid: 100,
+            //rdev: 0,
+            size: 0,
+            //blksize: 4096,
+            //blocks: 8,
+            atime: new Date(),
+            mtime: new Date(),
+            ctime: new Date(),
+            birthtime: new Date(),
+            isFile: function() { return true; },
+            isDirectory: function() { return false; },
+            isBlockDevice: function() { return false; },
+            isCharacterDevice: function() { return false; },
+            isSymbolicLink: function() { return false; },
+            isFIFO: function() { return false; },
+            isSocket: function() { return false; }
+          });
+        },
+        readFile: function(path, cb) {
+          return cb(null, '');
+        },
+        mkdir: function(path, mode, cb) {
+          // creating directories is not supported
+          var err = new Error('permission denied');
+          err.code = 'EACCES';
+          return cb(err);
+        }
+      });
     } else {
       failure();
     }
